df3b2d8d6402243a662a6df23b49b6c1711612b6
Erick Tut <87912114+Erick-Tut@users.noreply.github.com>
2025-05-13 20:10:08 -0600

    03.12.2025

---
+++ SEMANA_9/l9_ET_1292625/Ejercicios.cs
@@ -0,0 +1,107 @@
+using System;
+class Ejercicios{
+ static void Main() {
+/*
+ //Ejercicio 01
+
+ Console.Write("Ingrese el monto total: ");
+ double a = int.Parse(Console.ReadLine());
+
+
+ if(a >= 100)
+ {
+    double b = 0.20;
+    double r = (a - (a * b));
+    Console.Write("El total de la compra es de: " + r + ".");
+ }
+
+ if(a >= 50 && a <= 99)
+ {
+    double e = 0.10;
+    double r = (a - (a * e));
+    Console.Write("El total de la compra es de: " + r + ".");
+ } 
+
+ if(a < 50)
+ {
+    Console.Write("El total de la compra es de: " + a + ".");
+ }
+*/
+ // Ejercicio o2
+/*
+  static void Main() {
+
+ Console.Write("Ingrese la edad: ");
+ double a = int.Parse(Console.ReadLine());
+
+
+ if(a >= 18)
+ {
+    Console.Write("Acceso concedido.");
+ }
+
+ if(a < 18)
+ {
+    Console.Write("Acceso denegado.");
+ }
+ }
+*/
+
+//Ejercicio 03
+/*
+Console.Write("Ingrese un número: ");
+ double a = int.Parse(Console.ReadLine());
+
+ Console.Write("Ingrese otro número: ");
+ double b = int.Parse(Console.ReadLine());
+
+
+ if(a == b)
+ {
+    double c = (a * b);
+    Console.Write("La multiplicación de ambos números es de: " + c + ".");
+ }
+
+ if(a > b)
+ {
+    double c = (a - b);
+    Console.Write("La resta de ambos números es de: " + c + ".");
+ }
+
+  if(a < b)
+ {
+    double c = (a + b);
+    Console.Write("La suma de ambos números es de: " + c + ".");
+ }
+*/
+/*
+//Ejercicio 04
+ Console.Write("Ingrese el nombre del vendedor: ");
+ string a = Console.ReadLine();
+
+ Console.Write("Ingrese cuantos carros ha vendido en el mes: ");
+ double b = int.Parse(Console.ReadLine());
+
+ double s = 350;
+ double c = s + (15 * b);
+
+ if(b >= 15)
+ {
+    double sbr = c + 40;
+    double im = sbr * 0.05;
+    double snt = sbr - im;
+
+    Console.WriteLine("Su nombre es: " + a + "\nSu sueldo bruto es: " + sbr + "\nEl impuesto a pagar es de: " + im + "\nY su sueldo neto es de: " + snt);
+ }
+
+if(b < 15)
+ {
+    double sb = c;
+    double i = sb * 0.05;
+    double sn = sb - i;
+    
+    Console.WriteLine("Su nombre es: " + a + "\nSu sueldo bruto es: " + sb + "\nEl impuesto a pagar es de: " + i + "\nY su sueldo neto es de: " + sn);
+   }
+*/
+ }
+}
