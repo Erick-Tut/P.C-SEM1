cbc483ef4f5ed4ee895304cf2576667a840293e8
Erick Tut <87912114+Erick-Tut@users.noreply.github.com>
2025-05-13 20:02:33 -0600

    03.05.2025

---
+++ SEMANA_8/lab8/MiPrimerProyecto/Program.cs
@@ -0,0 +1,67 @@
+﻿/*Erik Carlos Omar Cucul Tut, 1292625*/
+using System;
+class Program {
+ static void Main() {
+
+/*Ejercicio 01*/
+ /*
+ Console.Write("¿Cómo te llamas? ");
+ string nombre = Console.ReadLine();
+
+ Console.WriteLine("Hola, " + nombre + " ¡Bienvenido a C#!");
+*/
+
+/*Ejercicio 02*/
+/*
+ int numeroEntero = 10;
+ double numeroDecimal = 3.14;
+ 
+ string texto = "Hola, C#";
+ 
+ DateTime fechaActual = DateTime.Now;
+ 
+ bool esMayor = true;
+ 
+ Console.WriteLine(numeroEntero);
+ Console.WriteLine(numeroDecimal);
+ Console.WriteLine(texto);
+ Console.WriteLine(fechaActual);
+ Console.WriteLine(esMayor);
+*/
+
+/*Ejercicio 03-a*/
+/*
+ int a = 10;
+ int b = 3;
+ Console.WriteLine("Suma: " + (a + b)); 
+ Console.WriteLine("Resta: " + (a - b)); 
+ Console.WriteLine("Multiplicación: " + (a * b)); 
+ Console.WriteLine("División: " + (a / (double)b)); 
+ Console.WriteLine("Potencia: " + Math.Pow(a, b)); 
+ Console.WriteLine("Módulo (resto): " + (a % b)); 
+*/
+
+/*Ejercicio 3-b*/
+/*
+ int x = 5;
+ int y = 8;
+ Console.WriteLine(x == y);
+ Console.WriteLine(x != y);
+ Console.WriteLine(x < y);
+ Console.WriteLine(x > y); 
+*/
+
+
+/*Ejercicio 03-c*/
+/*
+ int edad = 20;
+ bool esEstudiante = true;
+ 
+ Console.WriteLine(edad > 18 && esEstudiante); 
+
+ Console.WriteLine(edad > 18 || esEstudiante); 
+
+ Console.WriteLine(!esEstudiante); 
+*/
+ }
+}
