6120ef76e5c80a58bbc9be0eca3d233b97608c18
Erick Tut <87912114+Erick-Tut@users.noreply.github.com>
2025-05-13 20:31:03 -0600

    04.23.2026

---
+++ SEMANA_15/LABORATORIO_13_EJ2/l13_ET_1292625_EJ2/Ejercicio3.cs
@@ -0,0 +1,9 @@
+﻿Reflexión:
+
+- Como escribo?
+
+-- Yo suelo escribir de una manera muy ordenada y clara, suelo tener mucho cuidado con la ortografía, con la redacción clara y consisa, también con el uso de signos como las comas, puntos, etc. Suelo redactarla muy clara.
+
+- Como cambia mi voz (tanto escriba como hablada), dependiendo del entorno y el contexto en el que me encuentre?
+
+-- Suele cambiar en la manera de tono, explicación y la manera de pensar, cuando escribo suele ser de la misma estructura.
