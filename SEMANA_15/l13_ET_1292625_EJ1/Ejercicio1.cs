b748fc4f9921c1acfbd43f3c670d557c34d1a1a9
Erick Tut <87912114+Erick-Tut@users.noreply.github.com>
2025-05-13 20:40:51 -0600

    04.23.2025

---
+++ SEMANA_15/l13_ET_1292625_EJ1/Ejercicio1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Ejercicio1 {
+    static void Main(string[] args) {
+
+        Console.Write("Ingrese el número de DNI: ");
+        int numero = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Ingrese la letra del DNI: ");
+        char letra = Convert.ToChar(Console.ReadLine());
+
+        if (comprueba_letra_dni(numero, letra)) {
+
+            Console.WriteLine("Bienvenido");
+
+        }
+        else {
+
+            Console.WriteLine("Ha cometido Ud. un error");
+
+        }
+    }
+
+    static char letra_dni(int numero) {
+
+        string letras = "TRWAGMYFPDXBNJZSQVHLCKE";
+        int resto = numero % 23;
+        return letras[resto];
+
+    }
+
+    static bool comprueba_letra_dni(int numero, char letra) {
+
+        return letra_dni(numero) == letra;
+        
+    }
+}
---
+++ SEMANA_15/l13_ET_1292625_EJ3/Ejercicio2.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Ejercicio2 {
+    static void Main() {
+
+        string contraseñaCorrecta = "Contra000";
+        SolicitarContraseña(contraseñaCorrecta);
+
+    }
+
+    static void SolicitarContraseña(string contraseñaCorrecta) {
+
+        int intentos = 0;
+        bool accesoConcedido = false;
+
+        while (intentos < 3 && !accesoConcedido) {
+
+            Console.Write("Ingrese su contraseña: ");
+
+            string ingreso = Console.ReadLine();
+
+            if (!ValidarFormato(ingreso)) {
+
+                Console.WriteLine("Formato inválido.");
+
+            } else if (ingreso == contraseñaCorrecta) {
+
+                Console.WriteLine("Acceso permitido");
+                accesoConcedido = true;
+
+            } else {
+                
+                Console.WriteLine("Contraseña incorrecta.");
+
+            }
+
+            intentos++;
+        }
+
+        if (!accesoConcedido) {
+            Console.WriteLine("Acceso denegado");
+        }
+    }
+
+    static bool ValidarFormato(string contraseña) {
+
+        bool ContMayuscula = false;
+        bool ContNumero = false;
+
+        for (int i = 0; i < contraseña.Length; i++) {
+
+            if (contraseña[i] >= 'A' && contraseña[i] <= 'Z')
+                ContMayuscula = true;
+
+            if (contraseña[i] >= '0' && contraseña[i] <= '9')
+                ContNumero = true;
+                
+        }
+
+        return ContMayuscula && ContNumero;
+    }
+}
